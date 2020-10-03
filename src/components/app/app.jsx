584cd745938334db220254accaa3b4768145ccd5
Ivan Kashin <barbos1701@gmail.com>
2020-10-03 10:00:38 +0300

    Добавляет маршрутизацию в приложение

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import MainPage from "../main-page/main-page";
 
 const App = (props) => {
   const {movieCard} = props;
   return (
-    <MainPage movieCard={movieCard}/>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage movieCard={movieCard}/>
+        </Route>
+        <Route path="/login" exact>
+
+        </Route>
+        <Route path="/mylist" exact>
+
+        </Route>
+        <Route path="/films/:id" exact>
+
+        </Route>
+        <Route path="/films/:id/review" exact>
+
+        </Route>
+        <Route path="/player/:id" exact>
+
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
