33caa5588bd8597d99d2c18e754afc34fa6197e0
Ivan Kashin <barbos1701@gmail.com>
2020-10-09 09:20:41 +0300

    Подключает компонент списка карточек в компоненте главной страницы

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import FilmCard from "../film-card/film-card";
+import FilmCardList from "../film-card-list/film-card-list";
 
 const MainPage = (props) => {
   const {movieCard, films} = props;
@@ -104,14 +104,9 @@ const MainPage = (props) => {
             </li>
           </ul>
 
-          <div className="catalog__movies-list">
-            {films.map((film, i) => (
-              <FilmCard
-                key={`film-${i}`}
-                film={film}
-              />
-            ))}
-          </div>
+          <FilmCardList
+            films={films}
+          />
 
           <div className="catalog__more">
             <button className="catalog__button" type="button">Show more</button>
