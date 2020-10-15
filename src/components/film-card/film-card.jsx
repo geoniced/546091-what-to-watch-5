6b9eb58e254183b45b019a6565f9d61867272bf8
Ivan Kashin <barbos1701@gmail.com>
2020-10-15 20:44:57 +0300

    Переделывает компонент видеоплеера на классовый, дает ему возможность при наведении больше секунды показать видео, подключает его

--- src/components/film-card/film-card.jsx
+++ src/components/film-card/film-card.jsx
@@ -19,19 +19,19 @@ const FilmCard = (props) => {
   const {
     title,
     fullSizePoster,
+    video,
   } = props.film;
 
   return (
     <article className="small-movie-card catalog__movies-card">
-      <div className="small-movie-card__image">
-        <VideoPlayer
-          src={fullSizePoster}
-          poster={fullSizePoster}
-          width={CardVideoSize.WIDTH}
-          height={CardVideoSize.HEIGHT}
-          videoStyles={VIDEO_STYLES}
-        />
-      </div>
+      <VideoPlayer
+        src={video}
+        poster={fullSizePoster}
+        width={CardVideoSize.WIDTH}
+        height={CardVideoSize.HEIGHT}
+        videoStyles={VIDEO_STYLES}
+        isMuted
+      />
       <h3 className="small-movie-card__title">
         <Link to="/films/id" className="small-movie-card__link">{title}</Link>
       </h3>
--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -1,27 +1,98 @@
-import React from "react";
+import React, {createRef, PureComponent} from "react";
 import PropTypes from "prop-types";
 
-const VideoPlayer = (props) => {
-  const {
-    isMuted = false,
-    src,
-    poster,
-    width,
-    height,
-    videoStyles,
-  } = props;
+const HOVER_TIME_TO_PLAY = 1000;
 
-  return (
-    <video
-      src={src}
-      poster={poster}
-      muted={isMuted}
-      width={width}
-      height={height}
-      style={videoStyles}
-    />
-  );
-};
+class VideoPlayer extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPlaying: false,
+    };
+
+    this._videoRef = createRef();
+    this._hoverTimerId = null;
+
+    this._handleVideoMouseOver = this._handleVideoMouseOver.bind(this);
+    this._handleVideoMouseOut = this._handleVideoMouseOut.bind(this);
+  }
+
+  _handleVideoMouseOver() {
+    this._hoverTimerId = setTimeout(() => this.setState({
+      isPlaying: true,
+    }), HOVER_TIME_TO_PLAY);
+  }
+
+  _handleVideoMouseOut() {
+    clearTimeout(this._hoverTimerId);
+    this._hoverTimerId = null;
+
+    this.setState({
+      isPlaying: false,
+    });
+  }
+
+  componentDidMount() {
+    const video = this._videoRef.current;
+
+    video.onplay = () => this.setState({
+      isPlaying: true,
+    });
+
+    video.onpause = () => this.setState({
+      isPlaying: false,
+    });
+  }
+
+  componentWillUnmount() {
+    const video = this._videoRef.current;
+
+    video.onplay = null;
+    video.onpause = null;
+  }
+
+  render() {
+    const {
+      isMuted = false,
+      src,
+      poster,
+      width,
+      height,
+      videoStyles,
+    } = this.props;
+
+    return (
+      <div
+        className="small-movie-card__image"
+        onMouseOver={this._handleVideoMouseOver}
+        onMouseOut={this._handleVideoMouseOut}
+      >
+        <video
+          src={src}
+          poster={poster}
+          muted={isMuted}
+          width={width}
+          height={height}
+          style={videoStyles}
+          ref={this._videoRef}
+        />
+      </div>
+    );
+  }
+
+  componentDidUpdate() {
+    const video = this._videoRef.current;
+
+    if (this.state.isPlaying) {
+      video.play();
+    } else {
+      video.pause();
+      video.load();
+    }
+  }
+}
+
 
 VideoPlayer.propTypes = {
   isMuted: PropTypes.bool,
