b25ca465a93e71773aac386c8e95229d28014cbd
Ivan Kashin <barbos1701@gmail.com>
2020-10-15 16:39:12 +0300

    Дополняет компонент видео пропсами необходимыми для его отображения

--- src/components/film-card/film-card.jsx
+++ src/components/film-card/film-card.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {FilmTypes} from "../../prop-types-validations";
+import VideoPlayer from "../video-player/video-player";
+
+const CardVideoSize = {
+  WIDTH: 280,
+  HEIGHT: 175,
+};
+
+const VIDEO_STYLES = {
+  verticalAlign: `top`,
+  width: `100%`,
+  height: `100%`,
+  objectFit: `cover`,
+};
 
 const FilmCard = (props) => {
   const {
@@ -11,7 +24,13 @@ const FilmCard = (props) => {
   return (
     <article className="small-movie-card catalog__movies-card">
       <div className="small-movie-card__image">
-        <img src={fullSizePoster} alt={title} width="280" height="175" />
+        <VideoPlayer
+          src={fullSizePoster}
+          poster={fullSizePoster}
+          width={CardVideoSize.WIDTH}
+          height={CardVideoSize.HEIGHT}
+          videoStyles={VIDEO_STYLES}
+        />
       </div>
       <h3 className="small-movie-card__title">
         <Link to="/films/id" className="small-movie-card__link">{title}</Link>
--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const VideoPlayer = (props) => {
-  const {isMuted = false, src, poster} = props;
+  const {
+    isMuted = false,
+    src,
+    poster,
+    width,
+    height,
+    videoStyles,
+  } = props;
 
   return (
     <video
       src={src}
       poster={poster}
       muted={isMuted}
+      width={width}
+      height={height}
+      style={videoStyles}
     />
   );
 };
@@ -16,7 +26,10 @@ const VideoPlayer = (props) => {
 VideoPlayer.propTypes = {
   isMuted: PropTypes.bool,
   src: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  videoStyles: PropTypes.object,
 };
 
 export default VideoPlayer;
