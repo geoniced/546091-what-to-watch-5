d0c49eb6a41f1e8925486f71174cfa3e5565a018
Ivan Kashin <barbos1701@gmail.com>
2020-10-19 18:15:25 +0300

    Немного дописывает редьюсер

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,17 @@
+import {ActionType} from "./actions";
+
 const initialState = {
   genre: `all`,
   films: [],
 };
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_GENRE:
+      return state;
+    case ActionType.GET_FILMS_BY_GENRE:
+      return state;
+  }
   return state;
 };
 
