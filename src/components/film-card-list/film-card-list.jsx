93d42fef5b812352246d8b7e59cca367ace6202e
Ivan Kashin <barbos1701@gmail.com>
2020-10-21 15:04:00 +0300

    Вырезает верстку кнопки допоказа, выносит её в компонент и показывает её если есть что показывать в компоненте списка фильмов

--- src/components/film-card-list/film-card-list.jsx
+++ src/components/film-card-list/film-card-list.jsx
@@ -1,31 +1,43 @@
-import React, {PureComponent} from "react";
+import React, {Fragment, PureComponent} from "react";
 import {FilmTypes} from "../../prop-types-validations";
 import FilmCard from "../film-card/film-card";
 import withVideoPlayer from "../../hocs/with-video-player";
+import ShowMoreButton from "../show-more-button/show-more-button";
 
 const FilmCardWrapped = withVideoPlayer(FilmCard);
+
+const FILM_CARDS_PER_STEP = 8;
 
 class FilmCardList extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      activeCard: null,
+      // activeCard: null, // it is not needed...
+      shownCards: Math.min(FILM_CARDS_PER_STEP, this.props.films.length),
     };
   }
 
   render() {
     const {films} = this.props;
+    const {shownCards} = this.state;
+    const shownFilms = films.slice(0, shownCards);
+
+    console.log(shownFilms.length <= films.length);
 
     return (
-      <div className="catalog__movies-list">
-        {films.map((film, i) => (
-          <FilmCardWrapped
-            key={`film-${i}`}
-            film={film}
-          />
-        ))}
-      </div>
+      <Fragment>
+        <div className="catalog__movies-list">
+          {shownFilms.map((film, i) => (
+            <FilmCardWrapped
+              key={`film-${i}`}
+              film={film}
+            />
+          ))}
+        </div>
+        {shownFilms.length < films.length ? <ShowMoreButton /> : null}
+
+      </Fragment>
     );
   }
 }
--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -88,10 +88,6 @@ const MainPage = (props) => {
           <FilmCardList
             films={films}
           />
-
-          <div className="catalog__more">
-            <button className="catalog__button" type="button">Show more</button>
-          </div>
         </section>
 
         <footer className="page-footer">
