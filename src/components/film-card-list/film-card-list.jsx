78f80f30931e4e053394786597a483a501b223a8
Ivan Kashin <barbos1701@gmail.com>
2020-10-21 16:58:33 +0300

    Переносит информацию о показанных карточках на главной в стор, создает для этого экшены, события, выносит кнопку допоказа в компонент главной страницы, делает функциональным компонентом компонент списка карточек

--- src/components/film-card-list/film-card-list.jsx
+++ src/components/film-card-list/film-card-list.jsx
@@ -1,46 +1,24 @@
-import React, {Fragment, PureComponent} from "react";
+import React from "react";
 import {FilmTypes} from "../../prop-types-validations";
 import FilmCard from "../film-card/film-card";
 import withVideoPlayer from "../../hocs/with-video-player";
-import ShowMoreButton from "../show-more-button/show-more-button";
 
 const FilmCardWrapped = withVideoPlayer(FilmCard);
 
-const FILM_CARDS_PER_STEP = 8;
+const FilmCardList = (props) => {
+  const {films} = props;
 
-class FilmCardList extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      // activeCard: null, // it is not needed...
-      shownCards: Math.min(FILM_CARDS_PER_STEP, this.props.films.length),
-    };
-  }
-
-  render() {
-    const {films} = this.props;
-    const {shownCards} = this.state;
-    const shownFilms = films.slice(0, shownCards);
-
-    console.log(shownFilms.length <= films.length);
-
-    return (
-      <Fragment>
-        <div className="catalog__movies-list">
-          {shownFilms.map((film, i) => (
-            <FilmCardWrapped
-              key={`film-${i}`}
-              film={film}
-            />
-          ))}
-        </div>
-        {shownFilms.length < films.length ? <ShowMoreButton /> : null}
-
-      </Fragment>
-    );
-  }
-}
+  return (
+    <div className="catalog__movies-list">
+      {films.map((film, i) => (
+        <FilmCardWrapped
+          key={`film-${i}`}
+          film={film}
+        />
+      ))}
+    </div>
+  );
+};
 
 FilmCardList.propTypes = FilmTypes.films;
 
--- src/const.js
+++ src/const.js
@@ -1,1 +1,2 @@
 export const ALL_GENRES_FILTER = `All genres`;
+export const FILM_CARDS_PER_STEP = 8;
