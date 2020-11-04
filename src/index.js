abaa96f1659cb6e5cdfdfef3e1f209f6ea5c6a76
Ivan Kashin <barbos1701@gmail.com>
2020-11-04 09:52:30 +0300

    WIP. Подключает redux-thunk при конфигурировании хранилища

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
+import thunk from "redux-thunk";
 import App from "./components/app/app";
 import reviews from "./mocks/reviews";
 import rootReducer from "./store/reducers/root-reducer";
+import {createAPI} from "./services/api";
+import {fetchFilmList} from "./store/api-actions";
 
 const movieCard = {
   title: `The Grand Budapest Hotel`,
@@ -12,10 +15,16 @@ const movieCard = {
   releaseDate: 2014,
 };
 
+const api = createAPI(
+    // сюда передать коллбек при unauthorized
+);
+
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    applyMiddleware(thunk.withExtraArgument(api))
 );
+
+store.dispatch(fetchFilmList());
 
 ReactDOM.render(
     <Provider store={store}>
