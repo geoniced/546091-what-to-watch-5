028dbf743d2cfdbd3f9933a0d58dd577d63e1d1c
Ivan Kashin <barbos1701@gmail.com>
2020-11-06 18:40:48 +0300

    Добавляет асинхронные действия по проверке авторизации и отправки логина и пароля

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,6 +1,20 @@
-import {loadFilms} from "./actions";
+import {loadFilms, requireAuthorization} from "./actions";
+import {AuthorizationStatus} from "../const";
 
 export const fetchFilmList = () => (dispatch, _getStore, api) => (
   api.get(`/films`)
     .then(({data}) => dispatch(loadFilms(data)))
+);
+
+export const checkAuth = () => (dispatch, _getStore, api) => (
+  api.get(`/login`)
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
+    .catch((err) => {
+      throw err;
+    })
+);
+
+export const login = ({login: email, password}) => (dispatch, _getStore, api) => (
+  api.post(`/login`, {email, password})
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
 );
