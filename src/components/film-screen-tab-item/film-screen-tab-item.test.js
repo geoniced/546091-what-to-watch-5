c37739cd0bb582107547207f9e9dcec05a6c186b
Ivan Kashin <barbos1701@gmail.com>
2020-11-18 22:32:29 +0300

    Пишет снепшот тест для фильмскрина (не работает) и для FilmScreenTabItem

---
+++ src/components/film-screen-tab-item/film-screen-tab-item.test.js
@@ -0,0 +1,52 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {Tab} from "../../const";
+import {noop} from "../../test-data";
+import {FilmScreenTabItem} from "./film-screen-tab-item";
+
+describe(`FilmScreenTabItem render`, () => {
+  it(`renders FilmScreenTabItem component with active status and Details type`, () => {
+    const tree = renderer
+      .create(
+          <FilmScreenTabItem
+            isActive={true}
+            type={Tab.DETAILS}
+            title={`Details`}
+            onTabClick={noop}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`renders FilmScreenTabItem component without active status and Details type`, () => {
+    const tree = renderer
+      .create(
+          <FilmScreenTabItem
+            isActive={false}
+            type={Tab.DETAILS}
+            title={`Details`}
+            onTabClick={noop}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`renders FilmScreenTabItem component without active status and Overview type`, () => {
+    const tree = renderer
+      .create(
+          <FilmScreenTabItem
+            isActive={false}
+            type={Tab.OVERVIEW}
+            title={`Overview`}
+            onTabClick={noop}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/components/film-screen/film-screen.test.js
@@ -0,0 +1,31 @@
+// import React from "react";
+// import {BrowserRouter} from "react-router-dom";
+// import renderer from "react-test-renderer";
+// import {AuthorizationStatus} from "../../const";
+// import {filmListMock, mockReviews, noop} from "../../test-data";
+// import {FilmScreen} from "./film-screen";
+
+// it(`renders FilmScreen component`, () => {
+//   const tree = renderer
+//     .create(
+//         <BrowserRouter>
+//           <FilmScreen
+//             film={filmListMock[0]}
+//             films={filmListMock}
+//             authorizationStatus={AuthorizationStatus.AUTH}
+//             reviews={mockReviews}
+//             onPlayButtonClick={noop}
+//             loadReviews={noop}
+//           />
+//         </BrowserRouter>
+//     )
+//     .toJSON();
+
+//   expect(tree).toMatchSnapshot();
+// });
+
+/*
+Could not find "store" in the context of "Connect(UserBlock)".
+Either wrap the root component in a <Provider>, or pass a custom React context provider
+to <Provider> and the corresponding React context consumer to Connect(UserBlock) in connect options.
+ */
