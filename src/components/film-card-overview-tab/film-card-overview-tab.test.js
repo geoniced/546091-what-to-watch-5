953dcc64c0b5dd55e8a22b64a6c1e9af8392cc33
Ivan Kashin <barbos1701@gmail.com>
2020-11-18 22:13:38 +0300

    Добавляет снепшот тесты для Овервью и Ревьюс табов у фильма

---
+++ src/components/film-card-overview-tab/film-card-overview-tab.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {filmListMock} from "../../test-data";
+import FilmCardOverviewTab from "./film-card-overview-tab";
+
+describe(`FilmCardOverviewTab render`, () => {
+  it(`renders FilmCardOverviewTab component with the first mock data piece`, () => {
+    const tree = renderer
+      .create(
+          <FilmCardOverviewTab
+            film={filmListMock[0]}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`renders FilmCardOverviewTab component with another data`, () => {
+    const tree = renderer
+      .create(
+          <FilmCardOverviewTab
+            film={filmListMock[5]}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
