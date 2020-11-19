d7112471c3e3b7581190a0e7ddaef41029f10e94
Ivan Kashin <barbos1701@gmail.com>
2020-11-19 20:10:07 +0300

    Дописывает снепшот тесты MainPage

--- src/components/main-page/main-page.test.js
+++ src/components/main-page/main-page.test.js
@@ -1,28 +1,66 @@
-// import React from "react";
-// import {BrowserRouter} from "react-router-dom";
-// import renderer from "react-test-renderer";
-// import {filmListMock, movieCard, noop} from "../../test-data/test-data";
-// import {MainPage} from "./main-page";
+import React from "react";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import renderer from "react-test-renderer";
+import {mockedStore} from "../../test-data/store";
+import {filmListMock, movieCard, noop} from "../../test-data/test-data";
+import {MainPage} from "./main-page";
 
-// describe(`MainPage render`, () => {
-//   it(`renders MainPage component`, () => {
-//     const tree = renderer
-//       .create(
-//           <MainPage
-//             movieCard={movieCard}
-//             activeGenre={`Action`}
-//             films={filmListMock}
-//             shownFilmsCount={8}
-//             onPlayButtonClick={noop}
-//             onGenreChange={noop}
-//             onShowMoreButtonClick={noop}
-//           />
-//       )
-//       .toJSON();
+describe(`MainPage render`, () => {
+  it(`renders MainPage component with active genre eq Action`, () => {
+    const tree = renderer
+      .create(
+          <Provider store={mockedStore}>
+            <BrowserRouter>
+              <MainPage
+                movieCard={movieCard}
+                activeGenre={`Action`}
+                films={filmListMock}
+                shownFilmsCount={8}
+                onPlayButtonClick={noop}
+                onGenreChange={noop}
+                onShowMoreButtonClick={noop}
+              />
+            </BrowserRouter>
+          </Provider>,
+          {
+            createNodeMock: () => {
+              return {};
+            }
+          }
+      )
+      .toJSON();
 
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`renders MainPage component with active genre eq All genres and shownFilms eq 16`, () => {
+    const tree = renderer
+      .create(
+          <Provider store={mockedStore}>
+            <BrowserRouter>
+              <MainPage
+                movieCard={movieCard}
+                activeGenre={`All genres`}
+                films={filmListMock}
+                shownFilmsCount={16}
+                onPlayButtonClick={noop}
+                onGenreChange={noop}
+                onShowMoreButtonClick={noop}
+              />
+            </BrowserRouter>
+          </Provider>,
+          {
+            createNodeMock: () => {
+              return {};
+            }
+          }
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
 
 /*
 Could not find "store" in the context of "Connect(UserBlock)".
