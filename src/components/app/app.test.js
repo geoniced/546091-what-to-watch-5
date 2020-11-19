773c46cb79d6a3b1378c64242892fc886bed5b4d
Ivan Kashin <barbos1701@gmail.com>
2020-11-19 18:11:30 +0300

    Использует подключение к стору для App

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,30 +1,59 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import {movieCard} from "../../test-data";
-import {App} from "./app";
+import configureStore from "redux-mock-store";
+import {Provider} from "react-redux";
+import {filmListMock, movieCard} from "../../test-data";
+import App, {App as AppWithoutStore} from "./app";
 import {noop} from "../../test-data";
+import {NameSpace} from "../../store/reducers/root-reducer";
+import {AuthorizationStatus} from "../../const";
 
 describe(`App render`, () => {
-  // TODO: CONNECT TO STORE
-  // it(`renders App component`, () => {
-  //   const tree = renderer
-  //     .create(
-  //         <App
-  //           movieCard={movieCard}
-  //           films={[]}
-  //           isLoading={false}
-  //           loadFilmList={noop}
-  //         />
-  //     )
-  //     .toJSON();
+  const mockStore = configureStore([]);
+  let store = null;
+  let appComponent = null;
 
-  //   expect(tree).toMatchSnapshot();
+  // beforeEach(() => {
+  store = mockStore({
+    [NameSpace.DATA]: {
+      activeGenre: `All genres`,
+      films: filmListMock,
+      shownFilmsCount: 8,
+      isLoading: false,
+    },
+    [NameSpace.USER]: {
+      authorizationStatus: AuthorizationStatus.AUTH,
+    }
+  });
   // });
 
-  it(`renders App component's preloader`, () => {
+  store.dispatch = jest.fn();
+
+  appComponent = renderer.create(
+      <Provider store={store}>
+        <App
+          movieCard={movieCard}
+          isLoading={false}
+          films={[]}
+          loadFilmList={noop}
+        />
+      </Provider>,
+      {
+        createNodeMock: () => {
+          return {};
+        }
+      }
+  );
+
+  it(`renders App component`, () => {
+
+    expect(appComponent.toJSON()).toMatchSnapshot();
+  });
+
+  it(`renders App component's preloader (no store here)`, () => {
     const tree = renderer
       .create(
-          <App
+          <AppWithoutStore
             movieCard={movieCard}
             films={[]}
             isLoading={true}
