3d5f7e733e668bae7af69904f6c774c1cfa3acd6
Ivan Kashin <barbos1701@gmail.com>
2020-11-19 17:36:04 +0300

    Делает ХОК снепшот тесты для withActivePlayer более насыщенными

--- src/hocs/with-active-player/with-active-player.test.js
+++ src/hocs/with-active-player/with-active-player.test.js
@@ -1,16 +1,38 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import {MockComponent} from "../../test-data";
+import {filmListMock} from "../../test-data";
+// import {MockComponent} from "../../test-data";
 import withActivePlayer from "./with-active-player";
+
+const {previewImage, videoPreview} = filmListMock[0];
+
+const mockVideoPlayerSettings = {
+  previewImage,
+  videoPreview,
+  width: 280,
+  height: 175,
+  filmId: 1,
+};
+
+const MockComponent = (props) => {
+  return (
+    <div>
+      {props.renderPlayer(mockVideoPlayerSettings)}
+    </div>
+  );
+};
 
 const MockComponentWrapped = withActivePlayer(MockComponent);
 
 it(`renders withActivePlayer HOC correctly`, () => {
   const tree = renderer
     .create(
-        <MockComponentWrapped>
-          <React.Fragment />
-        </MockComponentWrapped>
+        <MockComponentWrapped />,
+        {
+          createNodeMock: () => {
+            return {};
+          }
+        }
     )
     .toJSON();
 
