09b0ac3b02daa51289ab91fb9ca1954419c7a945
Ivan Kashin <barbos1701@gmail.com>
2020-11-20 20:16:10 +0300

    Добавляет юнит тесты для ActionCreator

---
+++ src/store/actions.test.js
@@ -0,0 +1,64 @@
+import {
+  changeGenre,
+  ActionType,
+  increaseShownFilmCards,
+  resetShownFilmCards,
+  loadFilms,
+  loadReviewsForFilm,
+  requireAuthorization,
+  redirectToRoute,
+} from "./actions";
+import {filmListMock, mockReviews} from "../test-data/test-data";
+import { AuthorizationStatus } from "../const";
+
+describe(`Action creators work correctly`, () => {
+  it(`Action creator changeGenre works correctly`, () => {
+    expect(changeGenre(`Action`)).toEqual({
+      type: ActionType.CHANGE_GENRE,
+      payload: `Action`,
+    });
+  });
+
+  it(`Action creator increaseShownFilmCards works correctly`, () => {
+    expect(increaseShownFilmCards()).toEqual({
+      type: ActionType.INCREASE_SHOWN_FILM_CARDS,
+    });
+  });
+
+  it(`Action creator resetShownFilmCards works correctly`, () => {
+    expect(resetShownFilmCards()).toEqual({
+      type: ActionType.RESET_SHOWN_FILM_CARDS,
+    });
+  });
+
+  it(`Action creator loadFilms works correctly`, () => {
+    expect(loadFilms(filmListMock)).toEqual({
+      type: ActionType.LOAD_FILMS,
+      payload: filmListMock,
+    });
+  });
+
+  it(`Action creator loadReviewsForFilm works correctly`, () => {
+    expect(loadReviewsForFilm(mockReviews, 1)).toEqual({
+      type: ActionType.LOAD_REVIEWS_FOR_FILM,
+      payload: {
+        reviews: mockReviews,
+        filmId: 1,
+      },
+    });
+  });
+
+  it(`Action creator requireAuthorization works correctly`, () => {
+    expect(requireAuthorization(AuthorizationStatus.AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    });
+  });
+
+  it(`Action creator redirectToRoute works correctly`, () => {
+    expect(redirectToRoute(`/test-path`)).toEqual({
+      type: ActionType.REDIRECT_TO_ROUTE,
+      payload: `/test-path`,
+    });
+  });
+});
