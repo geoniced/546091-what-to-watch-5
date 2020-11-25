c2794a3c812ea6712fc4faf1e016882eef515e1f
Ivan Kashin <barbos1701@gmail.com>
2020-11-25 13:11:12 +0300

    Избавляется от класс компонента с жизненным циклом FilmScreen, переделывая его на хук

--- src/components/film-screen/film-screen.jsx
+++ src/components/film-screen/film-screen.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment, PureComponent} from "react";
+import React, {Fragment, useEffect} from "react";
 import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import {FilmTypes, ReviewTypes} from "../../prop-types-validations";
@@ -19,115 +19,122 @@ const FilmCardListWithActiveItem = withActivePlayer(FilmCardList);
 
 const SIMILIAR_FILMS_COUNT = 4;
 
-class FilmScreen extends PureComponent {
-  componentDidMount() {
-    const {loadReviews, film: {id: filmId}} = this.props;
-
+const useReviewsData = (filmId, loadReviews) => {
+  useEffect(() => {
     loadReviews(filmId);
-  }
+  }, []);
+};
 
-  render() {
-    const {onPlayButtonClick, reviews, film, films, authorizationStatus} = this.props;
+const FilmScreen = (props) => {
+  const {
+    onPlayButtonClick,
+    reviews,
+    film,
+    films,
+    authorizationStatus,
+    loadReviews,
+  } = props;
 
-    const {
-      id,
-      title,
-      genre,
-      releaseYear,
-      posterImage,
-      backgroundImage,
-      backgroundColor,
-    } = film;
+  const {
+    id,
+    title,
+    genre,
+    releaseYear,
+    posterImage,
+    backgroundImage,
+    backgroundColor,
+  } = film;
 
-    // Может тоже в пропсы выкинуть, чтобы компонент почище был? Надо подумать
-    const similarFilms = films
-      .filter((filmItem) => filmItem.title !== film.title)
-      .filter((filmItem) => filmItem.genre === film.genre)
-      .slice(0, SIMILIAR_FILMS_COUNT);
+  useReviewsData(id, loadReviews);
 
-    return (
-      <Fragment>
-        <section className="movie-card movie-card--full">
-          <div className="movie-card__hero">
-            <div className="movie-card__bg">
-              <img src={backgroundImage} alt={title} style={{backgroundColor}}/>
-            </div>
+  // Может тоже в пропсы выкинуть, чтобы компонент почище был? Надо подумать
+  const similarFilms = films
+    .filter((filmItem) => filmItem.title !== film.title)
+    .filter((filmItem) => filmItem.genre === film.genre)
+    .slice(0, SIMILIAR_FILMS_COUNT);
 
-            <h1 className="visually-hidden">WTW</h1>
+  return (
+    <Fragment>
+      <section className="movie-card movie-card--full">
+        <div className="movie-card__hero">
+          <div className="movie-card__bg">
+            <img src={backgroundImage} alt={title} style={{backgroundColor}}/>
+          </div>
 
-            <header className="page-header movie-card__head">
-              <LogoBlock />
-              <UserBlock />
-            </header>
+          <h1 className="visually-hidden">WTW</h1>
 
-            <div className="movie-card__wrap">
-              <div className="movie-card__desc">
-                <h2 className="movie-card__title">{title}</h2>
-                <p className="movie-card__meta">
-                  <span className="movie-card__genre">{genre}</span>
-                  <span className="movie-card__year">{releaseYear}</span>
-                </p>
+          <header className="page-header movie-card__head">
+            <LogoBlock />
+            <UserBlock />
+          </header>
 
-                <div className="movie-card__buttons">
-                  <button
-                    className="btn btn--play movie-card__button"
-                    type="button"
-                    onClick={onPlayButtonClick}
-                  >
-                    <svg viewBox="0 0 19 19" width="19" height="19">
-                      <use xlinkHref="#play-s"></use>
-                    </svg>
-                    <span>Play</span>
-                  </button>
-                  <button className="btn btn--list movie-card__button" type="button">
-                    <svg viewBox="0 0 19 20" width="19" height="20">
-                      <use xlinkHref="#add"></use>
-                    </svg>
-                    <span>My list</span>
-                  </button>
-                  {authorizationStatus === AuthorizationStatus.AUTH && (
-                    <Link to={`${AppRoute.FILMS}/${id}/review`} className="btn movie-card__button">
-                      Add review
-                    </Link>
-                  )}
-                </div>
+          <div className="movie-card__wrap">
+            <div className="movie-card__desc">
+              <h2 className="movie-card__title">{title}</h2>
+              <p className="movie-card__meta">
+                <span className="movie-card__genre">{genre}</span>
+                <span className="movie-card__year">{releaseYear}</span>
+              </p>
+
+              <div className="movie-card__buttons">
+                <button
+                  className="btn btn--play movie-card__button"
+                  type="button"
+                  onClick={onPlayButtonClick}
+                >
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button className="btn btn--list movie-card__button" type="button">
+                  <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use xlinkHref="#add"></use>
+                  </svg>
+                  <span>My list</span>
+                </button>
+                {authorizationStatus === AuthorizationStatus.AUTH && (
+                  <Link to={`${AppRoute.FILMS}/${id}/review`} className="btn movie-card__button">
+                    Add review
+                  </Link>
+                )}
               </div>
             </div>
           </div>
+        </div>
 
-          <div className="movie-card__wrap movie-card__translate-top">
-            <div className="movie-card__info">
-              <div className="movie-card__poster movie-card__poster--big">
-                <img src={posterImage} alt={title} width="218" height="327" />
-              </div>
-
-              <FilmScreenWithSwitchableTabs
-                film={film}
-                reviews={reviews}
-              />
+        <div className="movie-card__wrap movie-card__translate-top">
+          <div className="movie-card__info">
+            <div className="movie-card__poster movie-card__poster--big">
+              <img src={posterImage} alt={title} width="218" height="327" />
             </div>
+
+            <FilmScreenWithSwitchableTabs
+              film={film}
+              reviews={reviews}
+            />
           </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
+
+          <FilmCardListWithActiveItem films={similarFilms}/>
         </section>
 
-        <div className="page-content">
-          <section className="catalog catalog--like-this">
-            <h2 className="catalog__title">More like this</h2>
+        <footer className="page-footer">
+          <LogoBlock isFooter />
 
-            <FilmCardListWithActiveItem films={similarFilms}/>
-          </section>
-
-          <footer className="page-footer">
-            <LogoBlock isFooter />
-
-            <div className="copyright">
-              <p>© 2019 What to watch Ltd.</p>
-            </div>
-          </footer>
-        </div>
-      </Fragment>
-    );
-  }
-}
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    </Fragment>
+  );
+};
 
 FilmScreen.propTypes = {
   authorizationStatus: PropTypes.string,
