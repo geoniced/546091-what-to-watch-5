fc7564d6299d7d34605dc54f00a860e676bbf479
Ivan Kashin <barbos1701@gmail.com>
2020-11-25 09:52:01 +0300

    Переделывает компонент AuthScreen на функциональный

--- src/components/auth-screen/auth-screen.jsx
+++ src/components/auth-screen/auth-screen.jsx
@@ -1,85 +1,77 @@
-import React, {createRef, PureComponent} from "react";
+import React, {createRef} from "react";
 import PropTypes from "prop-types";
 import LogoBlock from "../logo-block/logo-block";
 import {connect} from "react-redux";
 import {login} from "../../store/api-actions";
 
-class AuthScreen extends PureComponent {
-  constructor(props) {
-    super(props);
+const AuthScreen = (props) => {
+  const emailRef = createRef();
+  const passwordRef = createRef();
 
-    this.emailRef = createRef();
-    this.passwordRef = createRef();
+  const {onSubmit} = props;
 
-    this._handleSubmit = this._handleSubmit.bind(this);
-  }
-
-  _handleSubmit(evt) {
-    const {onSubmit} = this.props;
-
+  const handleSubmit = (evt) => {
     evt.preventDefault();
 
     onSubmit({
-      email: this.emailRef.current.value,
-      password: this.passwordRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="user-page">
-        <header className="page-header user-page__head">
-          <LogoBlock />
-          <h1 className="page-title user-page__title">Sign in</h1>
-        </header>
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <LogoBlock />
+        <h1 className="page-title user-page__title">Sign in</h1>
+      </header>
 
-        <div className="sign-in user-page__content">
-          <form
-            onSubmit={this._handleSubmit}
-            action=""
-            className="sign-in__form"
-          >
-            <div className="sign-in__fields">
-              <div className="sign-in__field">
-                <input
-                  ref={this.emailRef}
-                  className="sign-in__input"
-                  type="email"
-                  placeholder="Email address"
-                  name="user-email"
-                  id="user-email"
-                />
-                <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
-              </div>
-              <div className="sign-in__field">
-                <input
-                  ref={this.passwordRef}
-                  className="sign-in__input"
-                  type="password"
-                  placeholder="Password"
-                  name="user-password"
-                  id="user-password"
-                />
-                <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
-              </div>
+      <div className="sign-in user-page__content">
+        <form
+          onSubmit={handleSubmit}
+          action=""
+          className="sign-in__form"
+        >
+          <div className="sign-in__fields">
+            <div className="sign-in__field">
+              <input
+                ref={emailRef}
+                className="sign-in__input"
+                type="email"
+                placeholder="Email address"
+                name="user-email"
+                id="user-email"
+              />
+              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
-            <div className="sign-in__submit">
-              <button className="sign-in__btn" type="submit">Sign in</button>
+            <div className="sign-in__field">
+              <input
+                ref={passwordRef}
+                className="sign-in__input"
+                type="password"
+                placeholder="Password"
+                name="user-password"
+                id="user-password"
+              />
+              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
-          </form>
-        </div>
-
-        <footer className="page-footer">
-          <LogoBlock isFooter />
-
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
           </div>
-        </footer>
+          <div className="sign-in__submit">
+            <button className="sign-in__btn" type="submit">Sign in</button>
+          </div>
+        </form>
       </div>
-    );
-  }
-}
+
+      <footer className="page-footer">
+        <LogoBlock isFooter />
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
 AuthScreen.propTypes = {
   onSubmit: PropTypes.func.isRequired,
